0df7552f8d17dcd9790151dbeb7bd6ee3a51866e
Charlotte W <152224795+108charlotte@users.noreply.github.com>
2025-12-07 14:53:15 -0500

    template for pygame final project

---
+++ session notes/pygame/temp.py
@@ -0,0 +1,35 @@
+import pygame
+
+pygame.init()
+
+
+screen_w = 640
+screen_h = 640
+
+screen = pygame.display.set_mode((screen_w, screen_h))
+
+pygame.display.set_caption("Adding Sprites")
+character = pygame.image.load("C:\\Users\\dawoo\\Downloads\\kenney_pixel-platformer-food-expansion\\Tiles\\tile_0103.png")
+dimensions = block_img.get_rect()
+
+block_h = dimensions.height
+
+# see bottom of game loop
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.blit(block_img, dimensions)
+        
+    screen.fill((255, 255, 255))
+    
+    pygame.display.flip()
+    
+    clock.tick(60)
+
+
+pygame.quit()
